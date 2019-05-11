863eb01eb081a0728a59a14cae98f24dbf5c90e1
b-m-f <max@ehlers.berlin>
2019-05-11 14:56:10 +0100

    Fixes post

--- api/freelytics/lib/freelytics/router.ex
+++ api/freelytics/lib/freelytics/router.ex
@@ -1,22 +1,31 @@
 defmodule Freelytics.Router do
   use Plug.Router
+  require Logger
 
+  plug(Plug.Parsers, parsers: [:json], json_decoder: Jason)
   plug(:match)
   plug(:dispatch)
-  plug(Plug.Parsers, parsers: [:urlencoded, :json], json_decoder: Jason)
 
   get "/get" do
-    analytics = %Freelytics.Analytics{}
-    Freelytics.Repo.insert(analytics)
     send_resp(conn, 200, "world")
   end
 
   post "/save" do
-    # analytics = %Freelytics.Analytics{}
+    {status, body} =
+      case conn.body_params do
+        %{"root" => root, "url" => url} -> {200, %{url: url, root: root}}
+        _ -> {422, "fails"}
+      end
+
+    Logger.info("got a post request for analytics to be saved")
+    Logger.info("#{inspect(body)}")
+
+    # here we should just update directly in the database
+    # without getting the value first and then counting up by 1
+    # {analytics, err} = Freelytics.Repo.get!(Freelytics.Analytics, root)
+    # IO.inspect(analytics)
     # Freelytics.Repo.insert(analytics)
-    {:ok, body, conn} = Plug.Conn.read_body(conn)
-    IO.inspect(body)
-    send_resp(conn, 200, "world")
+    send_resp(conn, 200, Jason.encode!(body))
   end
 
   match _ do
--- api/freelytics/priv/repo/migrations/20190511122210_analytics.exs
+++ api/freelytics/priv/repo/migrations/20190511122210_analytics.exs
@@ -3,7 +3,7 @@ defmodule Freelytics.Repo.Migrations.Analytics do
 
   def change do
     create table(:analytics) do
-      add(:root, :string, size: 40)
+      add(:root, :string, size: 40, primary_key: true)
       add(:url, :text)
       add(:times_visited, :integer)
       add(:time_spent, :float)
