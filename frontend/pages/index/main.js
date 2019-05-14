d2241517def0f1573eca9632f6502a654179ea94
b-m-f <max@ehlers.berlin>
2019-05-14 20:28:24 +0100

    Triggers fetch on press enter

--- frontend/pages/index/main.js
+++ frontend/pages/index/main.js
@@ -5,13 +5,15 @@ const urlinput = document.getElementById("urlInput");
 const getDataButton = document.getElementById("getDataButton");
 
 // The Api URL is defined in HTML and comes loaded from the template variables
-getDataButton.addEventListener("click", () =>{
-    const urlToGet = urlinput.value;
-    fetch(`${apiUrl}/get/${urlToGet}`)
-        .then(function (a) {
-            return a.json(); // call the json method on the response to get JSON
-        })
-        .then(function (json) {
-            ElmBuild.Elm.DashBoard.init({ node: contentDiv, flags: json });
-        });
+getDataButton.addEventListener("click", event => {
+  console.log(event);
+  event.preventDefault();
+  const urlToGet = urlinput.value;
+  fetch(`${apiUrl}/get/${urlToGet}`)
+    .then(function(a) {
+      return a.json(); // call the json method on the response to get JSON
+    })
+    .then(function(json) {
+      ElmBuild.Elm.DashBoard.init({ node: contentDiv, flags: json });
+    });
 });
