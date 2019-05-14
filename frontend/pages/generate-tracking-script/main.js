fa59eb5113a9d9ef45f7e05ac57cb0f971bea2bc
b-m-f <max@ehlers.berlin>
2019-05-14 20:37:57 +0100

    Removes old files, removes code highlighting. Wraps JavaScript in script tag

--- frontend/pages/generate-tracking-script/main.js
+++ frontend/pages/generate-tracking-script/main.js
@@ -1,45 +1,40 @@
-import {sendData} from "./tracking-script/main.js";
-import Prism from "prismjs";
+import { sendData } from "./tracking-script/main.js";
 
 const input = document.getElementById("customUrlInput");
 const button = document.getElementById("copyButton");
 const code = document.getElementById("output");
 
-
-function getTrackingScriptForDomain(domain){
-    return `(${sendData}("${domain}"))`;
+function getTrackingScriptForDomain(domain) {
+  return `&ltscript&gt
+  (${sendData}("${domain}"))
+  &lt/script&gt`;
 }
 
 code.innerHTML = getTrackingScriptForDomain(input.value);
 
-Prism.highlightElement(code, false, () => {});
-
 let executingInput = false;
-input.addEventListener("input", (e) => {
-
-    if (!executingInput){
-        executingInput = true;
-        window.setTimeout(() => {
-            const domain =  e.target.value;
-            executingInput = false;
-            code.innerHTML = getTrackingScriptForDomain(domain);
-            Prism.highlightElement(code, false, () => {});
-        }, 600);
-    }
+input.addEventListener("input", e => {
+  if (!executingInput) {
+    executingInput = true;
+    window.setTimeout(() => {
+      const domain = e.target.value;
+      executingInput = false;
+      code.innerHTML = getTrackingScriptForDomain(domain);
+    }, 600);
+  }
 });
 
-
 button.addEventListener("click", () => {
-    if (document.body.createTextRange) {
-        const range = document.body.createTextRange();
-        range.moveToElementText(code);
-        range.select();
-    } else if (window.getSelection) {
-        const selection = window.getSelection();
-        const range = document.createRange();
-        range.selectNodeContents(code);
-        selection.removeAllRanges();
-        selection.addRange(range);
-    }
-    document.execCommand("copy");
+  if (document.body.createTextRange) {
+    const range = document.body.createTextRange();
+    range.moveToElementText(code);
+    range.select();
+  } else if (window.getSelection) {
+    const selection = window.getSelection();
+    const range = document.createRange();
+    range.selectNodeContents(code);
+    selection.removeAllRanges();
+    selection.addRange(range);
+  }
+  document.execCommand("copy");
 });
