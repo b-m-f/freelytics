66ac7429c4b4df4fc59a6f1a1068c72864d2d8f0
b-m-f <max@ehlers.berlin>
2019-05-25 16:20:22 +0100

    Update tracking script to fall back to XHR if fetch is not supported

--- frontend/pages/generate-tracking-script/tracking-script/main.js
+++ frontend/pages/generate-tracking-script/tracking-script/main.js
@@ -1,22 +1,39 @@
 export function sendData(url) {
-  const l = document.createElement("a");
-  l.href = window.location.href;
+    const l = document.createElement("a");
+    l.href = window.location.href;
 
-  function getPath() {
-    return l.href;
-  }
+    function sendDataToApi(apiUrl, data){
+        const xmlhttp = new XMLHttpRequest();
+        xmlhttp.open("POST", apiUrl);
+        xmlhttp.setRequestHeader("Content-Type", "application/json");
+        xmlhttp.setRequestHeader("Accept", "application/json");
+        xmlhttp.send(data);
+    }
 
-  function getHost() {
-    return l.hostname;
-  }
-  const path = getPath();
-  const host = getHost();
-  fetch(`${url}/save`, {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({ url: path, root: host })
-  });
+    function getPath() {
+        return l.href;
+    }
+
+    function getHost() {
+        return l.hostname;
+    }
+
+    const path = getPath();
+    const host = getHost();
+    const body =  JSON.stringify({ url: path, root: host });
+    const apiUrl = `${url}/save`;
+
+    if("fetch" in window){
+        fetch(apiUrl, {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json"
+            },
+            body
+        });
+    }
+    else {
+        sendDataToApi(apiUrl, body);
+    }
 }
