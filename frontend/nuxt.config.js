b4ee33a376dd7d9cd228d3107be52331d7b653a1
b-m-f <max@ehlers.berlin>
2019-07-28 15:07:20 +0100

    Use purgecss to make css smaller => https://tailwindcss.com/docs/controlling-file-size"

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -1,3 +1,16 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  // Specify the paths to all of the template files in your project
+  content: [
+    './src/**/*.html',
+    './src/**/*.vue',
+    './src/**/*.jsx'
+    // etc.
+  ],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || []
+})
+
 module.exports = {
   mode: 'universal',
   /*
@@ -53,10 +66,12 @@ module.exports = {
    ** Build configuration
    */
   build: {
+    extractCSS: true,
     postcss: {
       plugins: [
         require('tailwindcss')('./tailwind.config.js'),
-        require('autoprefixer')
+        require('autoprefixer'),
+        ...(process.env.NODE_ENV === 'production' ? [purgecss] : [])
       ]
     },
     /*
